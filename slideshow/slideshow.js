1b7f65a03e85e0b642ffd8f12005bc2dc5cbc59c
Virginie Duboscq <mentzelia@hotmail.fr>
2018-11-13 11:47:16 -1000

    animation fadeIn et Out ok - redim images ok

--- slideshow/slideshow.js
+++ slideshow/slideshow.js
@@ -4,19 +4,26 @@
 var slideIndex = 1;
 showDivs(slideIndex);
 
+
 //fonction qui affiche la diapo demandée (selon index) et cache les autres
 var ensbleSlides = ensbleSlides;
 
 function showDivs(n) {
     var i;
-    var ensbleSlides = document.getElementsByClassName("elements_slide_slider1");
+    var ensbleSlides = $(".elements_slide_slider1");
     //si l'index est > au nombre d'élements -> remise à zero, sinon affiche la slide demandée
-    if (n > ensbleSlides.length) {slideIndex = 1} 
-    if (n < 1) {slideIndex = ensbleSlides.length} ;
+    if (n > ensbleSlides.length) {
+        slideIndex = 1
+    };
+    //si on est à la 1ere slide retour à la dernière
+    if (n < 1) {
+        slideIndex = ensbleSlides.length
+    };
     for (i = 0; i < ensbleSlides.length; i++) {
-        ensbleSlides[i].style.display = "none"; 
-    }
-    ensbleSlides[slideIndex-1].style.display = "block"; 
+        $(ensbleSlides[i]).hide(); 
+    };
+    
+    $(ensbleSlides[slideIndex - 1]).show();
 }
 
 //Fonction pour faire défiler manuellement le slider
@@ -36,26 +43,29 @@ function right(){
 
 
 //Déplacement au clic fleches droite et gauche de l'écran
-var buttonLeft = document.getElementById("button_left");
-var buttonRight = document.getElementById("button_right");
+var buttonLeft = $("#button_left");
+var buttonRight = $("#button_right");
 
-buttonLeft.addEventListener("click", left);
-buttonRight.addEventListener("click", right);
+buttonLeft.click(left);
+buttonRight.click(right);
 
 
 
 //Déplacement slider aux touches fleches droite et gauche clavier
-document.addEventListener("keydown", function(){
+function keydownLeft(){
     if(event.keyCode==37){
         left();
     }
-});
+};
 
-document.addEventListener("keydown", function(){
+function keydownRight(){
     if(event.keyCode==39){
         right();
     }
-});
+};
+
+$("html").keydown(keydownRight);
+$("html").keydown(keydownLeft);
 
 //Fonction défilement automatique
 var slideIndex = 0;
@@ -63,12 +73,30 @@ carousel();
 
 function carousel() {
     var i;
-    var ensbleSlides = document.getElementsByClassName("elements_slide_slider1");
+    var ensbleSlides = $(".elements_slide_slider1");
     for (i = 0; i < ensbleSlides.length; i++) {
-      ensbleSlides[i].style.display = "none"; 
-    }
+      $(ensbleSlides[i]).hide(); 
+    };
+    
     slideIndex++;
-    if (slideIndex > ensbleSlides.length) {slideIndex = 1} 
-    ensbleSlides[slideIndex-1].style.display = "block"; 
+    if (slideIndex > ensbleSlides.length) {slideIndex = 1}; 
+    
     setTimeout(carousel, 5000); // passe suivante toutes les 5s
-}
+    $(ensbleSlides[slideIndex - 1]).fadeIn(1000); 
+    $(ensbleSlides[slideIndex - 1]).delay(3000).fadeOut(1000);
+
+}; 
+
+    //Gestion bouton pause
+    var buttonPause= $(".fa-pause");
+
+    var sourisPause = buttonPause.hover(stopDefil, playDefil);
+    function stopDefil() {console.log("test entree");};
+    function playDefil() {console.log("test sortie");};
+
+
+   
+    
+    
+
+
