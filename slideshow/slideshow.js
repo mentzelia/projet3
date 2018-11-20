6906747f33c738cf7b5600204acf11c99512f7a7
Virginie Duboscq <mentzelia@hotmail.fr>
2018-11-20 11:35:28 -1000

    slider fonctionnel en POO

--- slideshow/slideshow.js
+++ slideshow/slideshow.js
@@ -1,137 +1,103 @@
-//Definition de mon objet
 var Slider = {
-    //Definition fonction initialisation
-     init: function(ensbleSlides, buttonLeft, buttonRight, buttonPause) {
-            this.ensbleSlides = ensbleSlides;
-            this.buttonLeft = buttonLeft;
-            this.buttonRight = buttonRight;
-            this.buttonPause = buttonPause;     
-     },
-    
-    //Definition fonction slide
+    //donner une valeur nulle aux propriétés
+    ensbleSlides: null,
+    buttonLeft: null,
+    buttonRight: null,
+    buttonPause: null,
+    buttonPlay: null,
+    slideIndex: 0,
+    timer: null,
+    duration: 5000,
+
+    init : function (ensbleSlides, buttonLeft, buttonRight, buttonPause, buttonPlay, duration) {
+
+        this.ensbleSlides = ensbleSlides;
+        this.buttonLeft = buttonLeft;
+        this.buttonRight = buttonRight;
+        this.buttonPause = buttonPause;
+        this.buttonPlay = buttonPlay;
+        if(duration){
+            this.duration = duration;
+        }
+        this.slide(); //méthode pour faire fonctionner le slider, évite de l'appeler à la creation de l'objet
+    },
+
     slide: function() {
-
-            //Définition et affichage 1ere slide
-            var slideIndex = 1;
-            this.showDivs(slideIndex);
-            
-            //Fonction pour faire défiler manuellement le slider
-            this.plusDivs(slideIndex);
-            //définition d'une fonction supplémentaire spécifique (+1, -1) pour l'intégrer ensuite dans un évenement
-            this.left();
-            this.right();
-
-            //Déplacement au clic fleches droite et gauche de l'écran
-            this.buttonLeft;
-            this.buttonRight;
-
-            this.buttonLeft.click(this.left);
-            this.buttonRight.click(this.right);
-
-/*
-            //Déplacement slider aux touches fleches droite et gauche clavier
-            this.keydownLeft();
-            this.keydownRight();
-        
-
-            $("html").keydown(keydownRight);
-            $("html").keydown(keydownLeft);
-*/
-            this.keydown();
-        
-            //Fonction défilement automatique
-            var slideIndex = 0;
-            //this.carousel();
+        this.buttonPlay.hide();
+        this.showDivs(this.slideIndex);//creation slider
+        this.buttonRight[0].addEventListener("click", this.right.bind(this));//bind permet de garder lien à right et pas au buttonRight, ne fonctionne pas avec jQuery, indice car considéré comme un tableau
+        this.buttonLeft[0].addEventListener("click", this.left.bind(this));
+        this.initCaroussel();//automatisation slider + play/pause
     },
 
-    //Definition fonction showDivs qui affiche la diapo demandée (selon index) et cache les autres au départ à 0
-    showDivs: function(n) {
-                var i;
-                this.ensbleSlides;
-                //si l'index est > au nombre d'élements -> remise à zero, sinon affiche la slide demandée
-                if (n > this.ensbleSlides.length) {
-                    slideIndex = 1
-                };
-                //si on est à la 1ere slide permet un retour à la dernière
-                if (n < 1) {
-                    slideIndex = this.ensbleSlides.length
-                };
-                for (i = 0; i < this.ensbleSlides.length; i++) {
-                    $(this.ensbleSlides[i]).hide(); 
-                };
+    showDivs: function (n) {
+        //si l'index est > au nombre d'élements -> remise à zero, sinon affiche la slide demandée
+        if ( n > this.ensbleSlides.length) {
+            this.slideIndex = 1
+        };
+        //si on est à la 1ere slide permet un retour à la dernière
+        if (n < 1) {
+            this.slideIndex = this.ensbleSlides.length
+        };
 
-                $(this.ensbleSlides[this.slideIndex - 1]).show();
+        for (var i = 0; i < this.ensbleSlides.length; i++) {
+            $(this.ensbleSlides[i]).hide(); 
+        };
+
+        $(this.ensbleSlides[this.slideIndex - 1]).show();
     },
 
-    //Definition fonction plusDivs
+    //Fonction pour faire défiler manuellement le slider
     plusDivs: function(n) {
-                this.showDivs(this.slideIndex += n);
+        this.showDivs(this.slideIndex += n);
     },
-    
-    //Definition fonction left
+
+    //définition d'une fonction supplémentaire spécifique (+1, -1) pour l'intégrer ensuite dans un évenement au clic
     left: function(){
-                Slider.plusDivs(-1);
+        this.plusDivs(-1);
     },
 
-    //Definition fonction right
-    right: function() {
-                Slider.plusDivs(+1);
+    right: function(){
+        this.plusDivs(+1);
     },
-
-    //Methode keydown
-    
-    keydown : function(){
-        $("html").keypress( function(event){
-            if(event.keyCode==37){
-                    this.left();
-                };
-            if(event.keyCode==39){
-                    this.right();
-                };
-        });   
-},
     
 
-    //Definition fonction carousel
-    carousel: function() {
-                var i;
-                this.ensbleSlides;
-                for (i = 0; i < this.ensbleSlides.length; i++) {
-                  $(this.ensbleSlides[i]).hide(); 
-                };
-
-                slideIndex++;
-                if (slideIndex > this.ensbleSlides.length) {slideIndex = 1}; 
-
-                var timer = setTimeout(carousel, 5000); // passe suivante toutes les 5s
-
-                $(this.ensbleSlides[slideIndex - 1]).fadeIn(2000); 
-                $(this.ensbleSlides[slideIndex - 1]).delay(3000);
-
-
-                //Gestion bouton pause
-                this.buttonPause;
-
-                var sourisPause = this.buttonPause.hover(stopDefil, playDefil);
-                function stopDefil() {
-                    clearTimeout(timer);
-                };
-
-                function playDefil() {
-                    setTimeout(carousel, 500);
-                };
+    //initialisation, lance le carousel, gestion des boutons play/pause
+    initCaroussel: function(){
+        this.carousel();
+        this.buttonPause.click(this.stopDefil.bind(this));//bind pour lier this à la methode et non au boutonPause
+        this.buttonPlay.click(this.playDefil.bind(this));
+    },
+    
+    carousel: function() {    
+        this.right(); 
+        this.timer = setTimeout(this.carousel.bind(this), this.duration); 
     },
 
-};
+    //permet de stopper le slider et modif aparition des boutons
+    stopDefil: function() {
+        clearTimeout(this.timer);
+        this.buttonPlay.show();
+        this.buttonPause.hide();
+    },
 
+    //permet de relancer le slider et modif aparition des boutons
+    playDefil: function() {
+        setTimeout(this.carousel.bind(this), this.duration);
+        this.buttonPlay.hide();
+        this.buttonPause.show();
+    }
 
-//Creation du nouvel objet
+}
+
+//Creation de l'objet
+var ensbleSlides = $(".elements_slide_slider1");
+var buttonLeft = $("#button_left");
+var buttonRight = $("#button_right");
+var buttonPause = $(".fa-pause");
+var buttonPlay = $(".fa-play");
+
 var slider1 = Object.create(Slider);
-slider1.init($(".elements_slide_slider1"),$("#button_left"),$("#button_right"),$(".fa-pause"));
-slider1.slide();
-
-
-
-
+slider1.init(ensbleSlides, buttonLeft, buttonRight, buttonPause, buttonPlay, 5000)
     
     
