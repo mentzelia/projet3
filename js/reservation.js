54ec1122aee494e2db2c3bcc1aa46d00ccc9c291
Virginie Duboscq <mentzelia@hotmail.fr>
2019-01-16 17:39:44 +1300

    objet reservation fonctionnel + erreur au get du session storage qui reste a corriger

--- js/reservation.js
+++ js/reservation.js
@@ -46,118 +46,133 @@ recupInfoStorage1.init("prenom", "nom");
 
 
 //3- Mecanisme d'enclenchement de la reservations selon conditions de verifications établies
-
-
-if (sessionStorage.getItem("statutReservation")=== "true"){
-    document.getElementById("button").addEventListener("click", function(e) {
-        e.preventDefault();
-    });
+var GestionnaireReservation = {
+    init: function(button, imageSignature, timer, prenom, nom, adresse, texteErreur, save){
+        this.button = document.getElementById(button);
+        this.image = document.getElementById(imageSignature);
+        this.elementHtmlSection = document.getElementById(timer);
+        this.prenom = document.getElementById(prenom);
+        this.nom = document.getElementById(nom);
+        this.adresse = document.getElementById(adresse);
+        this.texteErreur = document.getElementById(texteErreur);
+        this.saveButton = document.getElementById(save);
         
-    //Affichage Image signature sauvegardée dans session Storage
-    var image = document.getElementById("imageSignature");
-    image.src = signatureReservation;
-
-    //Objet texte timer
-    document.getElementById("timer").style.display = "flex";
-    var elementHtmlSection = document.getElementById("timer"); 
-    var reservationRafraichie = Object.create(TexteReservation);
-    reservationRafraichie.init(elementHtmlSection);
+        
+        
+        this.lancementReservation(button);        
+    },
     
-    //Récupération Date d'expiration réservation
-    var dateExpiration = Date.parse(sessionStorage.getItem("dateExp")); // dateExp est un string dans session Storage. Transforme en Date le string dateExp.
+    lancementReservation: function(button) {
+        if (sessionStorage.getItem("statutReservation")=== "true"){
+            this.button.addEventListener("click", function(e) {
+                e.preventDefault();
+            }.bind(this));
+            
+            //Affichage Image signature sauvegardée dans session Storage
+            this.image.src = this.signatureReservation;
+            
+            //Objet texte reservation au rafraichissement navigateur
+            this.elementHtmlSection.style.display = "flex";
+            this.reservationRafraichie = Object.create(TexteReservation);
+            this.reservationRafraichie.init(this.elementHtmlSection);
     
-    //Date raffraichissement
-    var dateRafraichissement = new Date();
+            //Récupération Date d'expiration réservation
+            this.dateExpiration = Date.parse(sessionStorage.getItem("dateExp")); // dateExp est un string dans session Storage. Transforme en Date le string dateExp.
+            
+            //Date raffraichissement
+            this.dateRafraichissement = new Date();
+
+            this.tpsRestantEnSeconde = (this.dateExpiration-this.dateRafraichissement)/1000; // De base en mS d'ou transfo en sec
+
+            this.minRestante = Math.floor(this.tpsRestantEnSeconde/60); // nombre entier
+            this.secRestante = Math.floor(this.tpsRestantEnSeconde%60); // modulo permet calcul restant d'une division
+
+            console.log("il reste "+ this.minRestante+"min et "+ this.secRestante+"sec.");
+
+            //Creation objet Timer lors du rafraichissement de la page    
+            this.timerRafraichi = Object.create(Timer);
+            this.timerRafraichi.init(this.minRestante, this.secRestante);
     
-    var tpsRestantEnSeconde = (dateExpiration-dateRafraichissement)/1000; // De base en mS d'ou transfo en sec
-    
-    var minRestante = Math.floor(tpsRestantEnSeconde/60); // nombre entier
-    var secRestante = Math.floor(tpsRestantEnSeconde%60); // modulo permet calcul restant d'une division
-    
-    console.log("il reste "+minRestante+"min et "+secRestante+"sec.");
+        } else {
+            
+            //Au clic sur Réserver: vérifie conditions + ouvre fenetre signature + enregistre données Nom/Prénom pour prochaine fois
+            this.button.addEventListener("click", function(e) {
+                e.preventDefault(); //empêche le navigateur de rafraichir
 
-    //Creation objet Timer lors du rafraichissement de la page    
-    var timerRafraichi = Object.create(Timer);
-    timerRafraichi.init(minRestante, secRestante);
-    
-} else {
+                if (sessionStorage.getItem("statutReservation") === null || sessionStorage.getItem("statutReservation") === "false") { 
+                    if (statut !== null) { 
+                        if (statut === "OUVERT") { 
+                            if (disponibilite === "OK") { 
+                                if (this.nom.value.length !==0) {
+                                    if (this.prenom.value.length !==0) {
+                                        this.texteErreur.textContent = " ";
+                                        //nom et prénom gardés en mémoire pour prochaine reservation
 
-    //Au clic sur Réserver: vérifie conditions + ouvre fenetre signature + enregistre données Nom/Prénom pour prochaine fois
-    document.getElementById("button").addEventListener("click", function(e) {
-        var prenom = document.getElementById("prenom");
-        var nom = document.getElementById("nom");
-        var adresse = document.getElementById("adresse");
+                                        localStorage.setItem("prenom", this.prenom.value);
+                                        localStorage.setItem("nom", this.nom.value);
 
-        e.preventDefault(); //empêche le navigateur de rafraichir
+                                        //Apparition section canva
+                                        document.getElementById("signatureDiv").style.display = "flex";
 
-        if (sessionStorage.getItem("statutReservation") === null || sessionStorage.getItem("statutReservation") === "false") { 
-            if (statut !== null) {     
-                if (statut === "OUVERT") {
-                    if (disponibilite === "OK") {
-                        if (nom.value.length !==0) {
-                            if (prenom.value.length !==0) {
-                                document.getElementById("texteErreur").textContent = " ";
-                                //nom et prénom gardés en mémoire pour prochaine reservation
-
-                                localStorage.setItem("prenom", prenom.value);
-                                localStorage.setItem("nom", nom.value);
-
-                                //Apparition section canva
-                                document.getElementById("signatureDiv").style.display = "flex";
-
+                                    } else {
+                                        this.texteErreur.textContent = "Veuillez renseigner votre prénom.";
+                                    }
+                                } else {
+                                    this.texteErreur.textContent = "Veuillez renseigner votre nom."; 
+                                } 
                             } else {
-                                document.getElementById("texteErreur").textContent = "Veuillez renseigner votre prénom.";
+                                this.texteErreur.textContent = "Il n'y a plus de vélos disponibles. Veuillez sélectionner une autre station.";   
                             }
                         } else {
-                            document.getElementById("texteErreur").textContent = "Veuillez renseigner votre nom."; 
-                        } 
+                            this.texteErreur.textContent = "Cette station n'est pas disponible. Veuillez en sélectionner une autre.";
+                        }
                     } else {
-                        document.getElementById("texteErreur").textContent = "Il n'y a plus de vélos disponibles. Veuillez sélectionner une autre station.";   
-                    }
-                } else {
-                    console.log(statut);
-                    document.getElementById("texteErreur").textContent = "Cette station n'est pas disponible. Veuillez en sélectionner une autre.";
-                }
-            } else {
-                document.getElementById("texteErreur").textContent = "Erreur. Veuillez sélectionner une station.";
-            };
-        }else {
-            console.log("Réservation en cours");
-            //il y a le timer en cours qui est sur la section de texte et qui se décompte 
+                        this.texteErreur.textContent = "Erreur. Veuillez sélectionner une station.";
+                    };
+                }else {
+                    console.log("Réservation en cours");
+                    //il y a le timer en cours qui est sur la section de texte et qui se décompte 
+                };
+
+            }.bind(this));
+            
+            //Appel canva, timer + texte
+            this.canvas1 = Object.create(Canvas);
+            this.canvas1.init("canvas", "#000", "1", "save", "clear", "signatureDiv");
+
+            this.saveButton.addEventListener("click", function(e) {
+                this.elementHtmlSection.style.display= "flex";
+
+                //enregistre temporairement signature + reservation (timer)
+                sessionStorage.setItem("signature", this.image.src);
+
+                //enregistrer date d'expiration de la reservation
+                this.dateClic = new Date();
+                this.dateExpiration = new Date (this.dateClic);
+                this.dateExpiration.setMinutes(this.dateClic.getMinutes() + 20);
+                sessionStorage.setItem("dateExp", this.dateExpiration);
+
+                //Objet texte reservation
+                this.reservation1 = Object.create(TexteReservation);
+                this.reservation1.init(this.elementHtmlSection);
+
+                //Objet timer
+                this.timer1 = Object.create(Timer);
+                this.timer1.init(1, 0);
+
+                //intégrer statut de la réservation dans SessionStorage pour pouvoir verifier statut au prochain clic Reserver
+                sessionStorage.setItem("statutReservation", true);
+            }.bind(this));
+               
         };
-
-    });
-
-    //Appel canva, timer+ texte
-
-    //Objet canva
-    var canvas1 = Object.create(Canvas);
-    canvas1.init("canvas", "#000", "1", "save", "clear", "signatureDiv");
-
-    document.getElementById("save").addEventListener("click", function(e) {
-        var img = document.getElementById("imageSignature");
-        
-        document.getElementById("timer").style.display= "flex";
-
-        //enregistre temporairement signature + reservation (timer)
-        sessionStorage.setItem("signature", img.src);
-        
-        //enregistrer date d'expiration de la reservation
-        var dateClic = new Date();
-        var dateExpiration = new Date (dateClic);
-        dateExpiration.setMinutes(dateClic.getMinutes() + 20);
-        sessionStorage.setItem("dateExp", dateExpiration);
-        
-        //Objet texte reservation
-        var elementHtmlSection = document.getElementById("timer"); 
-        var reservation1 = Object.create(TexteReservation);
-        reservation1.init(elementHtmlSection);
-
-        //Objet timer
-        var timer1 = Object.create(Timer);
-        timer1.init(1, 0);
-        
-        //intégrer statut de la réservation dans SessionStorage pour verifier statut au prochain clic Reserver
-        sessionStorage.setItem("statutReservation", true);
-    });
+    
+    },
 };
+
+var gestionnaireReservation1 = Object.create(GestionnaireReservation);
+gestionnaireReservation1.init("button", "imageSignature", "timer", "prenom", "nom", "adresse", "texteErreur", "save");
+
+
+            
+            
+            
